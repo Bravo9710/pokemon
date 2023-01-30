32725713edb21adf2c44395bcb45b07bbd4e9a47
vvenkov <vvenkov@htmlbox.net>
2023-01-30 23:45:55 +0200

    Fetch pokemons form pokemon api and add theirs names in list

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,20 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  const pokemonApiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10';
+
   const ul = document.querySelector("ul");
+
+  return new Promise((resolve, reject) => {
+    fetch(pokemonApiUrl)
+    .then((res) => {
+      res.json().then((res) => {
+        res.results.forEach((pokemon) => {
+          const li = document.createElement('li');
+          li.innerText = pokemon.name;
+          ul.appendChild(li);
+        })
+      });
+    })
+  })
 });
